e1c3d7f0024b29491d1943520838239455c42f8a
kdarwic <kdarwic@educacion.navarra.es>
2025-12-17 13:14:46 +0100

    subi los cambios

--- src/main/java/Trabajadores.java
+++ src/main/java/Trabajadores.java
@@ -1,17 +1,21 @@
 public class Trabajadores {
 
+    private static Trabajador[] trabajadores= {
+            new Trabajador("Aitor", 2),
+            new Trabajador("Alba", 2),
+            new Trabajador("Ander", 1),
+            new Trabajador("Laura", 1),
+            new Trabajador("Nerea", 1),
+            new Trabajador("Edurne", 0),
+            new Trabajador("Tasio", 0),
+            new Trabajador("Iosu", 0),
+    };
+
     public static Trabajador getTrabajador(int numero) {
-        switch (numero) {
-            case 1: return new Trabajador("Aitor", 2);
-            case 2: return new Trabajador("Alba", 2);
-            case 3: return new Trabajador("Ander", 1);
-            case 4: return new Trabajador("Laura", 1);
-            case 5: return new Trabajador("Nerea", 1);
-            case 6: return new Trabajador("Edurne", 0);
-            case 7: return new Trabajador("Tasio", 0);
-            case 8: return new Trabajador("Iosu", 0);
+        if (numero < 1 || numero > trabajadores.length) {
+            return null;
         }
-        return null;
+        return trabajadores[numero-1];
     }
 
     public static void mostrar() {
@@ -26,9 +30,16 @@ public class Trabajadores {
         //  6. Edurne    20 E.
         //  7. Tasio     20 E.
         //  8. Iosu      20 E.
+        for (int i = 0; i < trabajadores.length; i++) {
+            System.out.printf("%2d. %-8s %3d E.\n",
+                    i+1,
+                    trabajadores[i].getNombre(),
+                    trabajadores[i].getPrecioHora());
+        }
+    }
 
-
-
+    public static void main(String[] args) {
+        mostrar();
     }
 
 }
